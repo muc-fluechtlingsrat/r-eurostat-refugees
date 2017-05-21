041d3bd52e65e504b5380f68ac45be2d573b20fe
ynux <ynux@gmx.net>
2017-05-21 10:17:15 +0200

    removed old comment

--- R/import_packages.R
+++ R/import_packages.R
@@ -2,13 +2,13 @@
 
 ipak <- function(pkg){
     new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-    if (length(new.pkg)) 
+    if (length(new.pkg))
         install.packages(new.pkg, dependencies = TRUE)
     sapply(pkg, require, character.only = TRUE)
 }
 
-# usage
-packages <- c("ggplot2", "plyr", "reshape2", "RColorBrewer", "scales", "grid")
 list.of.packages <- c("tidyr", "dplyr", "magrittr", "ggplot2", "eurostat", "gridExtra", "lubridate")
 ipak(list.of.packages)
+
+lapply(list.of.packages, require, character.only = TRUE)
 
